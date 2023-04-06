9bd7ae67c9506cad1665c237c1116f8284232a0d
unknown <akbarfisabilillah6@gmail.com>
2023-04-06 15:22:13 +0700

    nmbh dile 2

--- app/config/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host = "localhost";
-$dbname = "zakat";
-$dbuser = "root";
-$dbpass = "";
-$opts = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
-$zkt = "mysql:host=$host;charset=utf8;dbname=$dbname;";
-$db = new PDO($zkt, $dbuser, $dbpass, $opts);
-?>
--- app/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'core/app.php';
-require_once 'core/controller.php';
-require_once 'core/database.php';
-require_once 'core/flasher.php';
-
-require_once 'config/config.php';
